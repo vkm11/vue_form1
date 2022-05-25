cc3a0d8a9263fcda1de0ff3a0d23ed85109e4935
vijaykumar <vijay1111mane@gmail.com>
2022-05-25 15:57:09 +0530

    commite;

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <h1>Make form in vue js</h1>
+  <Login />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Login from './components/Login'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Login
   }
 }
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input type="text" v-model="loginForm.username" name="username" placeholder="Enter user name"/><br /><br />
+    <input type="password" v-model="loginForm.password" name="password" placeholder="Enter password" /><br /><br />
+    <button v-on:click="loginData">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login-form",
+  methods: {
+    loginData() {
+      console.log("Login Data",this.loginForm.username,this.loginForm.password);
+    }
+  },
+  data() {
+      return {
+        loginForm: {
+          username: null,
+          password: null,
+        }
+      }
+    }
+}
+</script>
