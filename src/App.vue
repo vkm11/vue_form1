1cf70b6ea86a6ae29b90fc76b6707851e49f2734
vijaykumar <vijay1111mane@gmail.com>
2022-05-25 16:39:13 +0530

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-<div id="app">
-  <h1>Make form in vue js</h1>
-  <Login />
+  <div id="app">
+    <!-- <h1>Make form in vue js</h1> -->
+    <h2>form validation</h2>
+    <!-- <Login /> -->
+    <Login_validation />
   </div>
 </template>
 
 <script>
-
-import Login from './components/Login'
+// import Login from "./components/Login";
+import Login_validation from "./components/Login_validation";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-   Login
-  }
-}
+    // Login,
+    Login_validation
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Login_validation.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div v-if="error.length">
+            <h3>Please correct the following errors</h3>
+            <ul>
+                <li v-for="e in error" v-bind:key="e.id">
+                {{e}}
+                </li>
+            </ul>
+        </div>
+        <form @submit="login">
+            <input type="text" placeholder="Enter name" v-model="name"/> <br> <br>
+            <input type="password" placeholder="Enter password" v-model="password"/><br> <br>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+<script>
+
+export default {
+   name: "Login-validation",
+   data(){
+       return{ 
+           error:[],
+           name:null,
+           password:null,
+       }
+   },
+   methods: { 
+       login(e){
+           this.error=[];
+           if(this.name && this.password)
+           {
+               console.log("no error");
+              
+           }
+           if(!this.name){
+               this.error.push("user name is required");
+           }
+             if(!this.password){
+               this.error.push("password is required");
+           }
+           console.log("Hello",this.error);
+           e.preventDefault();
+       }
+   }
+}
+</script>
